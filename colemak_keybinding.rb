689fe47997a7fd9c6dcda5d37bca96f77b38f16f
isty2e <soinbae@gmail.com>
2024-07-18 07:35:14 +0900

    feat: more remaps

--- colemak_keybinding.rb
+++ colemak_keybinding.rb
@@ -1,16 +1,24 @@
-require 'json'
+require "json"
 
 # Define the key mappings for Colemak
 key_mappings = {
-  'j' => 'n',
-  'k' => 'e',
-  'l' => 'i',
-  'n' => 'k',
-  'i' => 'u',
-  'u' => 'l',
-  'e' => 'f',
-  'f' => 't',
-  't' => 'j'
+  "e" => "f",
+  "r" => "p",
+  "t" => "g",
+  "y" => "j",
+  "u" => "l",
+  "i" => "u",
+  "o" => "y",
+  "p" => ";",
+  "s" => "r",
+  "d" => "s",
+  "f" => "t",
+  "g" => "d",
+  "j" => "n",
+  "k" => "e",
+  "l" => "i",
+  ";" => "o",
+  "n" => "k"
 }
 
 # Function to swap keys according to the key_mappings
@@ -19,29 +27,29 @@ def colemak_friendly(bindings, key_mappings)
 
   bindings.each do |key, value|
     # Split the key by space to handle combinations like 'ctrl-w k'
-    parts = key.split(' ')
+    parts = key.split(" ")
 
     # Map each part of the combination using key_mappings
     new_parts = parts.map do |part|
       # Handle the case where '-' is a keybinding by itself
-      if part == '-'
-        '-'
+      if part == "-"
+        "-"
       else
-        part.split('-').map { |subpart| key_mappings.fetch(subpart, subpart) }.join('-')
+        part.split("-").map { |subpart| key_mappings.fetch(subpart, subpart) }.join("-")
       end
     end
 
     # Join the parts back together to form the new key
-    new_key = new_parts.join(' ')
+    new_key = new_parts.join(" ")
 
     # Recursively process nested hashes or arrays
     new_value = if value.is_a?(Hash)
-                  colemak_friendly(value, key_mappings)
-                elsif value.is_a?(Array)
-                  value.map { |v| v.is_a?(Hash) ? colemak_friendly(v, key_mappings) : v }
-                else
-                  value
-                end
+      colemak_friendly(value, key_mappings)
+    elsif value.is_a?(Array)
+      value.map { |v| v.is_a?(Hash) ? colemak_friendly(v, key_mappings) : v }
+    else
+      value
+    end
 
     new_bindings[new_key] = new_value
   end
@@ -50,18 +58,18 @@ def colemak_friendly(bindings, key_mappings)
 end
 
 # Read the JSON configuration from a file
-file_path = 'vim.json'
+file_path = "vim.json"
 system("curl -L https://raw.githubusercontent.com/zed-industries/zed/master/assets/keymaps/vim.json > #{file_path}")
 json_data = File.read(file_path)
 config = JSON.parse(json_data)
 
 # Apply the key swaps
 config.each do |entry|
-  entry['bindings'] = colemak_friendly(entry['bindings'], key_mappings)
+  entry["bindings"] = colemak_friendly(entry["bindings"], key_mappings)
 end
 
 # Apply the user key bindings from user.json
-user_file_path = 'user.json'
+user_file_path = "user.json"
 if File.exist?(user_file_path)
   user_json_data = File.read(user_file_path)
   user_config = JSON.parse(user_json_data)
@@ -69,6 +77,5 @@ if File.exist?(user_file_path)
   config.concat(user_config)
 end
 
-
 # Write the modified configuration back to a file or print it
-File.write('colemak_keybindings.json', JSON.pretty_generate(config))
+File.write("colemak_keybindings.json", JSON.pretty_generate(config))
